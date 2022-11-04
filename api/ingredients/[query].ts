1724d92237dfe41f0a74423ed6402b66bab66806
ivo2666 <ivailocvetkov365@gmail.com>
2022-11-04 11:53:48 +0200

    fix: dynamic paths

--- api/ingredients/[query].ts
+++ api/ingredients/[query].ts
@@ -5,12 +5,13 @@ const API = process.env.REACT_APP_API_URL;
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 export default async function handler(request:VercelRequest, response:VercelResponse) {
-  const query = Object.entries(request.query).map(([key,value]) => `${key}=${value}`).join("&");
-  const res: AxiosResponse = await axios({
+  const query = request.query.query;
+  const res:AxiosResponse = await axios({
     method: "get",
-    url: `${API}/recipes/findByIngredients?ingredients=${query}&apiKey=${API_KEY}`,
+    url: `${API}/food/ingredients/search?query=${query}&apiKey=${API_KEY}`,
     headers: { "Content-Type": "application/json" },
   });
+
   const { data } = res;
   return response.status(200).json(data);
 }
--- api/recipes/[ingredients].ts
+++ api/recipes/[ingredients].ts
@@ -5,13 +5,12 @@ const API = process.env.REACT_APP_API_URL;
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 export default async function handler(request:VercelRequest, response:VercelResponse) {
-  const query = Object.entries(request.query).map(([key,value]) => `${key}=${value}`).join("&");
-  const res:AxiosResponse<{data: any}> = await axios({
+  const ingredients = request.query.ingredients;
+  const res: AxiosResponse = await axios({
     method: "get",
-    url: `${API}/food/ingredients/search?query=${query}&apiKey=${API_KEY}`,
+    url: `${API}/recipes/findByIngredients?ingredients=${ingredients}&apiKey=${API_KEY}`,
     headers: { "Content-Type": "application/json" },
   });
-
   const { data } = res;
   return response.status(200).json(data);
 }
