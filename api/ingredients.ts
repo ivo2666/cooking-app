429538bc9aa880d35605386b68db0a6f32998048
ivo2666 <ivailocvetkov365@gmail.com>
2022-11-04 10:51:20 +0200

    fix: query destructuring

--- api/ingredients.ts
+++ api/ingredients.ts
@@ -5,7 +5,7 @@ const API = process.env.REACT_APP_API_URL;
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 export default async function handler(request:VercelRequest, response:VercelResponse) {
-  const {query} = request;
+  const query = Object.entries(request.query).map(([key,value]) => `${key}=${value}`).join("&");
   const res:AxiosResponse = await axios({
     method: "get",
     url: `${API}/food/ingredients/search?query=${query}&apiKey=${API_KEY}`,
