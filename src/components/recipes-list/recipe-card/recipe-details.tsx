f073399b4a8da2116a93552abee86138fd9ee680
ivo2666 <ivailocvetkov365@gmail.com>
2022-12-14 15:03:05 +0200

    fix: recipe details loading

--- src/components/recipes-list/recipe-card/recipe-details.tsx
+++ src/components/recipes-list/recipe-card/recipe-details.tsx
@@ -8,16 +8,13 @@ const RecipeDetails: React.FC<{ id: string }> = ({ id }) => {
     () => service.getRecipe(id)
   );
 
-  if (!recipe) {
-    return <Spinner />;
-  }
 
   return (
     <div className="p-5 prose lg:prose-xl ">
       {isRecipesLoading ? (
         <div className="flex items-center justify-center w-full min-h-[300px]"><Spinner /></div>
       ) : (
-        <p dangerouslySetInnerHTML={{ __html: recipe.summary }}></p>
+        <p dangerouslySetInnerHTML={{ __html: recipe?.summary || "" }}></p>
       )}
     </div>
   );
